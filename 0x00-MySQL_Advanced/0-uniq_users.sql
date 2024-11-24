a3a45982000bc2d76c689b29192d0e96998fc8bb
mahmoudramzy1 <mahmoud.ahmed.ramzy@gmail.com>
2024-11-24 23:50:35 +0200

    p

---
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -0,0 +1,7 @@
+-- SQL script that creates a table users
+
+CREATE TABLE IF NOT EXISTS users (
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    email VARCHAR(100) NOT NULL UNIQUE,
+    name VARCHAR(255)
+);
